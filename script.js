9122f83d6bf5f3545a78bd6b8472cf8439688e06
uko05 <yosieyosie1@yahoo.co.jp>
2026-01-21 00:08:44 +0900

    Move index.html to the root directory for GitHub Pages

--- script.js
+++ script.js
@@ -558,7 +558,7 @@ function saveImage() {
           // 「同キャラだけ」連打を止める（キャラ別クールダウン）
           // 全キャラまとめて止めたいなら key を固定文字にしてOK:
           // const key = 'bakatareLastSent';
-          const key = `bakatareLastSent`;
+          const key = `bakatareLastSent02`;
 
           const last = Number(localStorage.getItem(key) || 0);
           const nowMs = Date.now();
